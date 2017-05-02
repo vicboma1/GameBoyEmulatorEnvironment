bca626837261fd7936a06e1f26892655e882d2e3
vicboma1 <victorbolinchesmarin@gmail.com>
2017-05-02 19:22:50 +0200

    Define propertie maps to options in menuBar

--- src/main/kotlin/app/configuration/ConfigurationImpl.kt
+++ src/main/kotlin/app/configuration/ConfigurationImpl.kt
@@ -3,19 +3,22 @@ package src.configuration
 import app.components.MenuChooser
 import app.components.MenuDialog
 import app.components.MenuFile
+import app.components.menuBar.view.MenuOptions
 import app.components.menuBar.view.MenuView
 import app.components.panel.ContentPaneParentImpl
 import assets.frame.Frame
 import assets.progressBar.MenuBarImpl
 import assets.progressBar.StatusBarImpl
 import main.kotlin.utils.listGames.ListGames
-import utils.thread.CustomExecutor
 import java.awt.BorderLayout
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Created by vicboma on 02/12/16.
  */
 class ConfigurationImpl internal constructor(private val classLoader: ClassLoader, private val frame : Frame) {
+
+    val properties = ConcurrentHashMap<String,Any?>()
 
     /*************** SCREEN ***************/
 
@@ -36,9 +39,7 @@ class ConfigurationImpl internal constructor(private val classLoader: ClassLoade
                 classLoader,
                 frame,
                 statusBar,
-                CustomExecutor.instance.add {
-
-                }
+                properties
         )
     }
 
@@ -51,9 +52,11 @@ class ConfigurationImpl internal constructor(private val classLoader: ClassLoade
                                     MenuBarImpl.MenuFile(frame),
                                     MenuBarImpl.MenuDialog(frame),
                                     MenuBarImpl.MenuChooser(frame),
-                                    MenuBarImpl.MenuView(frame, contentPaneParent )
+                                    MenuBarImpl.MenuView(frame, contentPaneParent ),
+                                    MenuBarImpl.MenuOptions(frame, properties )
                             )
                     )
+
     }
 
     companion object {
