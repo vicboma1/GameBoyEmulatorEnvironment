52c5758217cde4ed8539ff2d23891ac17575b72d
vicboma1 <victorbolinchesmarin@gmail.com>
2017-05-09 21:48:24 +0200

    Replace MenuItem callback by suspend invoke for the menuItems

--- src/main/kotlin/app/components/menuBar/View/MenuView.kt
+++ src/main/kotlin/app/components/menuBar/View/MenuView.kt
@@ -2,19 +2,21 @@ package app.components.menuBar.view
 
 import app.components.panel.ContentPaneParentImpl
 import assets.frame.Frame
-import assets.menuBar.child.dialog.*
-import assets.progressBar.Menu
+import assets.menuBar.child.dialog.MenuBarViewGrid
+import assets.menuBar.child.dialog.MenuBarViewList
 import assets.progressBar.JMenuBarImpl
+import assets.progressBar.Menu
 import assets.progressBar.MenuImpl
+import assets.progressBar.StatusBar
 
 /**
  * Created by vicboma on 02/01/17.
  */
-fun JMenuBarImpl.Companion.MenuView(frame : Frame, contentPaneParentImpl : ContentPaneParentImpl): Menu {
+fun JMenuBarImpl.Companion.MenuView(frame : Frame, statusBar : StatusBar, contentPaneParentImpl : ContentPaneParentImpl): Menu {
 
     return MenuImpl.create("View")
             .apply{
-                addMenuItem(JMenuBarImpl.MenuBarViewGrid(frame,contentPaneParentImpl))
-                addMenuItem(JMenuBarImpl.MenuBarViewList(frame,contentPaneParentImpl))
+                addMenuItem(JMenuBarImpl.MenuBarViewGrid(frame,statusBar,contentPaneParentImpl))
+                addMenuItem(JMenuBarImpl.MenuBarViewList(frame,statusBar,contentPaneParentImpl))
             }
 }
